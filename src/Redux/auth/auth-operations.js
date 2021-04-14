eb51b0e9b3847a1cd8e2a2bbecd1bb3885901057
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-04-14 22:02:44 +0300

    ошибка too many redirects

--- src/Redux/auth/auth-operations.js
+++ src/Redux/auth/auth-operations.js
@@ -3,12 +3,21 @@ import * as authActions from './auth-actions';
 
 axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com/';
 
-const token = {};
+const token = {
+    set(token) {
+        axios.defaults.headers.common.Authorization = 'Bearer ${token}';
+    },
+    unset() {
+        axios.defaults.headers.common.Authorization = '';
+    },
+};
 export const register = (credentials) => async (dispatch) => {
     dispatch(authActions.registerRequest());
 
     try {
         const response = await axios.post('/users/signup', credentials);
+
+        token.set(response.data.token);
         dispatch(authActions.registerSuccess(response.data));
     } catch (error) {
         dispatch(authActions.registerError(error.message));
@@ -19,12 +28,25 @@ export const login = (credentials) => async (dispatch) => {
 
     try {
         const response = await axios.post('/users/login', credentials);
+
+        token.set(response.data.token);
         dispatch(authActions.loginSuccess(response.data));
     } catch (error) {
         dispatch(authActions.loginError(error.message));
     }
 };
-export const logout = (credentials) => (dispatch) => {};
+export const logout = () => async (dispatch) => {
+    dispatch(authActions.logoutRequest());
+
+    try {
+        await axios.post('/users/logout');
+
+        token.unset();
+        dispatch(authActions.logoutSuccess());
+    } catch (error) {
+        dispatch(authActions.logoutError(error.message));
+    }
+};
 export const getCurrentUser = () => (dispatch, getState) => {};
 
 // export default { register, login, logout, getCurrentUser };
--- src/Redux/auth/auth-selectors.js
+++ src/Redux/auth/auth-selectors.js
@@ -1,4 +1,3 @@
-export const getIsAuth = (state) => state.auth.token
-// export const getIsLoggedIn = (state) => state.auth.isLogin
-export const getUserName = (state) => state.auth.user.name
-// export const getUserEmail = (state) => state.auth.user.email
+export const getIsAuth = (state) => state.auth.token;
+
+export const getUserName = (state) => state.auth.user.name;
