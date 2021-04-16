a68c1a3c98df4dbeec849d3e3ec2b2a2f83af430
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-04-17 00:27:04 +0300

    добавляет стили

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as operations from '../../Redux/contacts/operations';
 import { getFilteredContacts } from '../../Redux/contacts/selectors';
+import styles from './Contacts.module.css';
 
 const ContactItem = ({ id, name, number, onRemove }) => {
     return (
         <li id={id}>
-            <span>{name}</span>: <span>{number}</span>
+            <span className={styles.contactsLabel}>{name}</span>:{' '}
+            <span className={styles.contactsLabel}>{number}</span>
             <button type="button" id={id} onClick={() => onRemove(id)}>
                 Delete
             </button>
@@ -16,17 +18,19 @@ const ContactItem = ({ id, name, number, onRemove }) => {
 
 const Contacts = ({ contacts, onRemove }) => {
     return (
-        <ul>
-            {contacts.map(({ id, name, number }) => (
-                <ContactItem
-                    key={id}
-                    id={id}
-                    name={name}
-                    number={number}
-                    onRemove={onRemove}
-                />
-            ))}
-        </ul>
+        <div className={styles.contactsDiv}>
+            <ul>
+                {contacts.map(({ id, name, number }) => (
+                    <ContactItem
+                        key={id}
+                        id={id}
+                        name={name}
+                        number={number}
+                        onRemove={onRemove}
+                    />
+                ))}
+            </ul>
+        </div>
     );
 };
 
